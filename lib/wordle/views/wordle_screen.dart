7ad66a03ab881f38a342f8f1f6af8403758a81e6
Djumabaevs <djumabaevb@gmail.com>
2022-06-29 17:20:59 +0600

    word model indexes

--- lib/wordle/views/wordle_screen.dart
+++ lib/wordle/views/wordle_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_examples/wordle/wordle.dart';
+
+enum GameStatus { playing, submitting, lost, won }
 
 class WordleScreen extends StatefulWidget {
   WordleScreen({Key? key}) : super(key: key);
@@ -8,6 +11,18 @@ class WordleScreen extends StatefulWidget {
 }
 
 class _WordleScreenState extends State<WordleScreen> {
+  GameStatus _gameStatus = GameStatus.playing;
+
+  final List<Word> _board = List.generate(
+    6,
+    (_) => Word(letters: List.generate(5, (_) => Letter.empty())),
+  );
+
+  int _currentWordIndex = 0;
+
+  Word? get _currentWord =>
+      _currentWordIndex < _board.length ? _board[_currentWordIndex] : null;
+
   @override
   Widget build(BuildContext context) {
     return Container();
