9ecbfaabcaa98e169007bd3e9da7d7aaac30e3b0
Djumabaevs <djumabaevb@gmail.com>
2022-06-29 13:22:25 +0600

    word model

---
+++ lib/wordle/views/wordle_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WordleScreen extends StatefulWidget {
+  WordleScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WordleScreen> createState() => _WordleScreenState();
+}
+
+class _WordleScreenState extends State<WordleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
