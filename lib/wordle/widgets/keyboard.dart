2f06edbaa228531107a2e10eae4eac725a074bfa
Djumabaevs <djumabaevb@gmail.com>
2022-06-30 11:30:57 +0600

    iterable row

--- lib/wordle/widgets/keyboard.dart
+++ lib/wordle/widgets/keyboard.dart
@@ -1,5 +1,88 @@
+import 'package:flutter/material.dart';
+
 const _qwerty = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-  ['Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DEL'],
+  ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DEL'],
 ];
+
+class Keyboard extends StatelessWidget {
+  const Keyboard({
+    Key? key,
+    required this.onKeyTapped,
+    required this.onDeleteTapped,
+    required this.onEnterTapped,
+  }) : super(key: key);
+
+  final void Function(String) onKeyTapped;
+  final VoidCallback onDeleteTapped;
+  final VoidCallback onEnterTapped;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: _qwerty.map(
+        (keyRow) => Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: keyRow.map(
+            (letter) {
+              if (letter == 'DEL') {
+                return _KeyboardButton.delete(onTap: onDeleteTapped);
+              } else if (letter == 'ENTER') {
+                return _KeyboardButton.enter(onTap: onEnterTapped);
+              }
+              return _KeyboardButton(
+                onTap: () => onKeyTapped(letter),
+                letter: letter,
+                backgroundColor: Colors.grey,
+              );
+            },
+          ).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class _KeyboardButton extends StatelessWidget {
+  const _KeyboardButton({
+    Key? key,
+    this.height = 48,
+    this.width = 30,
+    required this.onTap,
+    required this.backgroundColor,
+    required this.letter,
+  }) : super(key: key);
+
+  factory _KeyboardButton.delete({
+    required VoidCallback onTap,
+  }) =>
+      _KeyboardButton(
+        width: 56,
+        onTap: onTap,
+        backgroundColor: Colors.grey,
+        letter: 'DEL',
+      );
+
+  factory _KeyboardButton.enter({
+    required VoidCallback onTap,
+  }) =>
+      _KeyboardButton(
+        width: 56,
+        onTap: onTap,
+        backgroundColor: Colors.grey,
+        letter: 'ENTER',
+      );
+
+  final double height;
+  final double width;
+  final VoidCallback onTap;
+  final Color backgroundColor;
+  final String letter;
+
+  @override
+  Widget build(BuildContext context) {
+    return ();
+  }
+}
